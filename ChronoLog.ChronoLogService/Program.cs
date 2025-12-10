78b32f7bd8c0b1162ec53186ae62cb20cad9c641
Felix Heinke <felix@heinke.eu>
2025-12-10 22:28:38 +0100

    feat: integrate Radzen components into the application

--- ChronoLog.ChronoLogService/Program.cs
+++ ChronoLog.ChronoLogService/Program.cs
@@ -8,6 +8,7 @@ using ChronoLog.SqlDatabase;
 using ChronoLog.SqlDatabase.Context;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +33,7 @@ builder.Services.AddLogging(options =>
 });
 
 builder.Services.AddHeaderPropagation(o => { o.Headers.Add("x-auth-request-access-token"); });
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
