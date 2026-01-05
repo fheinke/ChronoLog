336e85a9c0d2962bca30b46b48a8e26054429967
Felix Heinke <felix@heinke.eu>
2026-01-05 22:08:09 +0100

    feat: add GermanProvince enum for regional categorization

--- ChronoLog.Core/Models/Enums.cs
+++ ChronoLog.Core/Models/Enums.cs
@@ -22,3 +22,25 @@ public enum ReducingWorkdayType
     Gleitzeittag,
     Feiertag
 }
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum GermanProvince
+{
+    ALL, // National
+    BW, // Baden-Württemberg
+    BY, // Bayern
+    BE, // Berlin
+    BB, // Brandenburg
+    HB, // Bremen
+    HH, // Hamburg
+    HE, // Hessen
+    MV, // Mecklenburg-Vorpommern
+    NI, // Niedersachsen
+    NW, // Nordrhein-Westfalen
+    RP, // Rheinland-Pfalz
+    SL, // Saarland
+    SN, // Sachsen
+    ST, // Sachsen-Anhalt
+    SH, // Schleswig-Holstein
+    TH  // Thüringen
+}
