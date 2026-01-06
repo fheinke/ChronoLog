0c9b18631ad37527a334ca929aedace633da81c0
Felix Heinke <felix@heinke.eu>
2026-01-06 07:41:43 +0100

    feat: enhance ProjectEntity and ProjectModel with validation attributes and max length constraints

--- ChronoLog.Core/Models/DisplayObjects/ProjectModel.cs
+++ ChronoLog.Core/Models/DisplayObjects/ProjectModel.cs
@@ -1,11 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ChronoLog.Core.Models.DisplayObjects;
 
 public class ProjectModel
 {
     public Guid ProjectId { get; set; }
-    public bool IsDefault { get; set; }
-    public string Name { get; set; }
-    public string? Description { get; set; }
-    public string ResponseObject { get; set; }
-    public string? DefaultResponseText { get; set; }
+
+    [Required] public bool IsDefault { get; set; }
+    [Required] [MaxLength(256)] public string Name { get; set; } = string.Empty;
+    [MaxLength(1024)] public string? Description { get; set; }
+    [Required] [MaxLength(256)] public string ResponseObject { get; set; } = string.Empty;
+    [MaxLength(1024)] public string? DefaultResponseText { get; set; }
 }
--- ChronoLog.SqlDatabase/Models/ProjectEntity.cs
+++ ChronoLog.SqlDatabase/Models/ProjectEntity.cs
@@ -1,14 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ChronoLog.SqlDatabase.Models;
 
 public class ProjectEntity
 {
+    [Key]
     public Guid ProjectId { get; set; }
     
-    public virtual ICollection<ProjecttimeEntity> Projecttimes { get; set; }
+    public virtual ICollection<ProjecttimeEntity> Projecttimes { get; set; } = new List<ProjecttimeEntity>();
     
+    [Required]
     public bool IsDefault { get; set; }
-    public required string Name { get; set; }
+    
+    [Required]
+    [MaxLength(256)]
+    public string Name { get; set; } = string.Empty;
+    
+    [MaxLength(1024)]
     public string? Description { get; set; }
-    public required string ResponseObject { get; set; }
+    
+    [Required]
+    [MaxLength(256)]
+    public string ResponseObject { get; set; } = string.Empty;
+    
+    [MaxLength(1024)]
     public string? DefaultResponseText { get; set; }
 }
