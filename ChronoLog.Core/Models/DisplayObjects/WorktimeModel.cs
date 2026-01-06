f6fb4515aedeefa32947fe433876658aeb805fa3
Felix Heinke <felix@heinke.eu>
2026-01-06 07:48:51 +0100

    feat: enhance WorktimeEntity and WorktimeModel with validation attributes and EndTime validation logic

--- ChronoLog.Core/Models/DisplayObjects/WorktimeModel.cs
+++ ChronoLog.Core/Models/DisplayObjects/WorktimeModel.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ChronoLog.Core.Models.DisplayObjects;
 
 public class WorktimeModel
 {
     public Guid WorktimeId { get; set; }
-    public Guid WorkdayId { get; set; }
-    public TimeOnly StartTime { get; set; }
+    [Required] public Guid WorkdayId { get; set; }
+    [Required] public TimeOnly StartTime { get; set; }
     public TimeOnly? EndTime { get; set; }
     public TimeSpan? BreakTime { get; set; }
 }
--- ChronoLog.SqlDatabase/Models/WorktimeEntity.cs
+++ ChronoLog.SqlDatabase/Models/WorktimeEntity.cs
@@ -1,13 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ChronoLog.SqlDatabase.Models;
 
+[CustomValidation(typeof(WorktimeEntity), nameof(ValidateEndTime))]
 public class WorktimeEntity
 {
-    public Guid WorktimeId { get; set; }
-    
-    public Guid WorkdayId { get; set; }
-    public WorkdayEntity Workday { get; set; }
-    
-    public TimeOnly StartTime { get; set; }
+    [Key] public Guid WorktimeId { get; set; }
+
+    [Required] public Guid WorkdayId { get; set; }
+
+    [ForeignKey(nameof(WorkdayId))] public WorkdayEntity Workday { get; set; } = null!;
+
+    [Required] public TimeOnly StartTime { get; set; }
     public TimeOnly? EndTime { get; set; }
     public TimeSpan? BreakTime { get; set; }
+
+    public static ValidationResult? ValidateEndTime(WorktimeEntity entity, ValidationContext context)
+    {
+        if (entity.EndTime.HasValue && entity.EndTime < entity.StartTime)
+            return new ValidationResult("EndTime cannot be earlier than StartTime.");
+        return ValidationResult.Success;
+    }
 }
