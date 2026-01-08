23e767eb1342d8f7ab90f0070232dfa96b36b6e6
Felix Heinke <felix@heinke.eu>
2026-01-08 11:16:56 +0100

    feat: add initial migration for database schema with Employees, Projects, Workdays, Projecttimes, and Worktimes tables

---
+++ ChronoLog.SqlDatabase/Migrations/20260108101629_Initial.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChronoLog.SqlDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    ObjectId = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false),
+                    Email = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false),
+                    Name = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: true),
+                    Province = table.Column<int>(type: "int", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "tinyint(1)", nullable: true),
+                    IsProjectManager = table.Column<bool>(type: "tinyint(1)", nullable: true),
+                    VacationDaysPerYear = table.Column<int>(type: "int", nullable: false),
+                    DailyWorkingTimeInHours = table.Column<double>(type: "double", nullable: false),
+                    OvertimeCorrectionInHours = table.Column<double>(type: "double", nullable: false),
+                    LastSeen = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjectId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    IsDefault = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false),
+                    Description = table.Column<string>(type: "varchar(1024)", maxLength: 1024, nullable: true),
+                    ResponseObject = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false),
+                    DefaultResponseText = table.Column<string>(type: "varchar(1024)", maxLength: 1024, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Workdays",
+                columns: table => new
+                {
+                    WorkdayId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workdays", x => x.WorkdayId);
+                    table.ForeignKey(
+                        name: "FK_Workdays_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Projecttimes",
+                columns: table => new
+                {
+                    ProjecttimeId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    WorkdayId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    TimeSpent = table.Column<TimeSpan>(type: "time(6)", nullable: false),
+                    ResponseText = table.Column<string>(type: "varchar(1024)", maxLength: 1024, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projecttimes", x => x.ProjecttimeId);
+                    table.ForeignKey(
+                        name: "FK_Projecttimes_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Projecttimes_Workdays_WorkdayId",
+                        column: x => x.WorkdayId,
+                        principalTable: "Workdays",
+                        principalColumn: "WorkdayId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Worktimes",
+                columns: table => new
+                {
+                    WorktimeId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    WorkdayId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    EndTime = table.Column<TimeSpan>(type: "time", nullable: true),
+                    BreakTime = table.Column<TimeSpan>(type: "time(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Worktimes", x => x.WorktimeId);
+                    table.ForeignKey(
+                        name: "FK_Worktimes_Workdays_WorkdayId",
+                        column: x => x.WorkdayId,
+                        principalTable: "Workdays",
+                        principalColumn: "WorkdayId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projecttimes_ProjectId",
+                table: "Projecttimes",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projecttimes_WorkdayId",
+                table: "Projecttimes",
+                column: "WorkdayId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workdays_EmployeeId",
+                table: "Workdays",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Worktimes_WorkdayId",
+                table: "Worktimes",
+                column: "WorkdayId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projecttimes");
+
+            migrationBuilder.DropTable(
+                name: "Worktimes");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Workdays");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
