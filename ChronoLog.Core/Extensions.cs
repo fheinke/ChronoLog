5132b238c8962cd579be9402ef8f9dcb6a05dabe
Felix Heinke <felix@heinke.eu>
2026-01-11 20:49:20 +0100

    feat: add extension methods for WorkdayType to determine working and non-working days

---
+++ ChronoLog.Core/Extensions.cs
@@ -0,0 +1,26 @@
+using ChronoLog.Core.Models;
+
+namespace ChronoLog.Core;
+
+public static class Extensions
+{
+    extension(WorkdayType type)
+    {
+        public bool IsWorkingDay() => type switch
+        {
+            WorkdayType.Homeoffice => true,
+            WorkdayType.Office => true,
+            WorkdayType.Dienstreise => true,
+            _ => false
+        };
+
+        public bool IsNonWorkingDay() => type switch
+        {
+            WorkdayType.Krankheitstag => true,
+            WorkdayType.Urlaub => true,
+            WorkdayType.Gleitzeittag => true,
+            WorkdayType.Feiertag => true,
+            _ => false
+        };
+    }
+}
