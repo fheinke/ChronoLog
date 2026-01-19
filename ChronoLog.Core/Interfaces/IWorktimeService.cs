535f4c26d23e62400ca02c6da8d7cc3be36da510
Felix Heinke <felix@heinke.eu>
2026-01-19 08:56:00 +0100

    refactor: enhance Worktime API with new endpoints and request models

--- ChronoLog.Core/Interfaces/IWorktimeService.cs
+++ ChronoLog.Core/Interfaces/IWorktimeService.cs
@@ -4,10 +4,8 @@ namespace ChronoLog.Core.Interfaces;
 
 public interface IWorktimeService
 {
-    Task<bool> CreateWorktimeAsync(WorktimePostModel worktime);
     Task<Guid> CreateWorktimeAsync(WorktimeModel worktime);
     Task<List<WorktimeModel>> GetWorktimesAsync();
-    Task<List<WorktimeModel>> GetWorktimesAsync(List<Guid> worktimeIds);
     Task<List<WorktimeModel>> GetWorktimesAsync(DateTime startDate, DateTime endDate);
     Task<WorktimeModel?> GetWorktimeAsync(Guid worktimeId);
     Task<bool> UpdateWorktimeAsync(WorktimeModel worktime);
