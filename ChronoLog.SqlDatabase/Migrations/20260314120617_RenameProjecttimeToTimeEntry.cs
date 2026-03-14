4b98f0b7e647c19cb620896e721324740c96ae1c
Felix Heinke <felix@heinke.eu>
2026-03-14 13:20:08 +0100

    refactor: rename Projecttime to TimeEntry and update related database schema

---
+++ ChronoLog.SqlDatabase/Migrations/20260314120617_RenameProjecttimeToTimeEntry.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChronoLog.SqlDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameProjecttimeToTimeEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Projecttimes",
+                newName: "TimeEntries");
+
+            migrationBuilder.RenameColumn(
+                name: "ProjecttimeId",
+                table: "TimeEntries",
+                newName: "TimeEntryId");
+
+            migrationBuilder.Sql(
+                "ALTER TABLE `TimeEntries` CHANGE `TimeSpent` `Duration` time(6) NOT NULL DEFAULT '00:00:00';");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Projecttimes_ProjectId",
+                table: "TimeEntries",
+                newName: "IX_TimeEntries_ProjectId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Projecttimes_WorkdayId",
+                table: "TimeEntries",
+                newName: "IX_TimeEntries_WorkdayId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "TimeEntries",
+                newName: "Projecttimes");
+
+            migrationBuilder.RenameColumn(
+                name: "TimeEntryId",
+                table: "Projecttimes",
+                newName: "ProjecttimeId");
+
+            migrationBuilder.Sql(
+                "ALTER TABLE `Projecttimes` CHANGE `Duration` `TimeSpent` time(6) NOT NULL DEFAULT '00:00:00';");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TimeEntries_ProjectId",
+                table: "Projecttimes",
+                newName: "IX_Projecttimes_ProjectId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TimeEntries_WorkdayId",
+                table: "Projecttimes",
+                newName: "IX_Projecttimes_WorkdayId");
+        }
+    }
+}
